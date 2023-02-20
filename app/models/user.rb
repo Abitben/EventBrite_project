c4a91f665b5bf484268ca53e97bbd77d5fd97daa
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-02-20 15:24:08 +0100

    try to push heroku attempt 1

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  after_create :welcome_send
+  
+  has_many :attendances
+  has_many :attendances, foreign_key: 'admin_id', class_name: "Attendance"
+  has_many :events, through: :attendances
+
+  
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
+
+end
